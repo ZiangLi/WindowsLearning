0383b5e7f84459c86fdab7c575ecdf60618a3233
ziangli <lzaatsysu@163.com>
2015-12-30 15:51:40 +0800

    myFirst

---
+++ WinAPILearning/firstWin.cpp
@@ -0,0 +1,27 @@
+#include <Windows.h>
+
+LRESULT CALLBACK wndPro(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	static TCHAR szAppName[] = TEXT("MyWindowsProgram");
+
+	HWND		hwnd;
+	MSG			msg;
+	WNDCLASSEX	wndclass;
+
+	wndclass.lpfnWndProc = wndPro;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(DKGRAY_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = szAppName;
+
+
+}
